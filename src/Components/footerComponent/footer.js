90f1f5743e339eb313a47e5a6b85ff65b8f5e7bb
mvashishth <vashishthmayur@gmail.com>
2020-09-25 21:16:34 +0530

    Removed clipping of text at the bottom when navbar is at the bottom of the screen

--- src/Components/footerComponent/footer.js
+++ src/Components/footerComponent/footer.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React from 'react'; 
+import "../../Assets/css/footer.css"
 
 
 function Footer() {
   return (
+  	<html>
+  	<body>
+  	<main style={{zIndex:"10"}}>
 
 
-<footer>
-</footer>
-
-
+<div className="col-lg-4 col-lg-offset-4 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+<div>
+<ul className="ul-footer footer-custom-nav">
+<li>
+mayur
+</li>
+<li>
+123
+</li>
+</ul>
+</div>
+</div>
+</main>
+</body>
+</html>
   );
 }
 
 export default Footer;
+ 
--- src/Components/pages/homepage.js
+++ src/Components/pages/homepage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../../Assets/css/default.min.css"
+import "../../Assets/css/homepage.css"
 import mainimage from "../../Assets/Images/Home/main.jpg"
 
 
@@ -7,12 +7,11 @@ function Homepage() {
   return (
   	<html>
   	<body>
-  	<main style={{zIndex:"10"}}>
+  	<main style={{zIndex:"10"}} className="homepage-main">
  <div className="col-lg-4 col-lg-offset-4 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 align-items-center ">
  <img src={mainimage} className="photo" />
  </div>
-<div className="col-lg-4 col-lg-offset-4 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 align-items-center text-justify">
-
+<div className="col-lg-4 col-lg-offset-4 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 align-items-center text-justify bottom-scroll">
 What is Lorem Ipsum?
 
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
@@ -23,7 +22,6 @@ remaining essentially unchanged. It was popularised in the 1960s with the releas
 sheets containing Lorem Ipsum passages, and more recently with
  desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu
 </div>
-
 
 </main>
 

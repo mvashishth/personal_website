b09ac727eba13d6e3196d30061475e4fefb517bb
mvashishth <vashishthmayur@gmail.com>
2020-09-26 23:39:26 +0530

    centred footer

--- src/Components/footerComponent/footer.js
+++ src/Components/footerComponent/footer.js
@@ -10,50 +10,15 @@ function Footer() {
 
  <div className="col-lg-4 col-lg-offset-4 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 footer-custom-nav" >
 <div className="footer-content">
-
-  <svg height="30px" id="Layer_1" version="1.1" viewBox="0 0 16 16" width="30px"
-  fill="currentColor" >
-  <path d="M15.45,7L14,5.551V2c0-0.55-0.45-1-1-1h-1c-0.55,0-1,0.45-1,1v0.553L9,0.555C8.727,0.297,8.477,0,8,0S7.273,0.297,7,0.555
-    L0.55,7C0.238,7.325,0,7.562,0,8c0,0.563,0.432,1,1,1h1v6c0,0.55,0.45,1,1,1h3v-5c0-0.55,0.45-1,1-1h2c0.55,0,1,0.45,1,1v5h3  c0.55,0,1-0.45,1-1V9h1c0.568,0,1-0.437,1-1C16,7.562,15.762,.325,15.45" 
-  className="fa-secondary"></path></svg>
-
+<div>
+Mayur
 </div>
-
-
-
-
-<div className="footer-content">
-
-  <svg height="30px" id="Layer_1" version="1.1" viewBox="0 0 16 16" width="30px"
-  fill="currentColor" >
-  <path d="M15.45,7L14,5.551V2c0-0.55-0.45-1-1-1h-1c-0.55,0-1,0.45-1,1v0.553L9,0.555C8.727,0.297,8.477,0,8,0S7.273,0.297,7,0.555
-    L0.55,7C0.238,7.325,0,7.562,0,8c0,0.563,0.432,1,1,1h1v6c0,0.55,0.45,1,1,1h3v-5c0-0.55,0.45-1,1-1h2c0.55,0,1,0.45,1,1v5h3  c0.55,0,1-0.45,1-1V9h1c0.568,0,1-0.437,1-1C16,7.562,15.762,.325,15.45" 
-  className="fa-secondary"></path></svg>
-
+<div>
+Mayur
 </div>
-
-
-<div className="footer-content">
-
-  <svg height="30px" id="Layer_1" version="1.1" viewBox="0 0 16 16" width="30px"
-  fill="currentColor" >
-  <path d="M15.45,7L14,5.551V2c0-0.55-0.45-1-1-1h-1c-0.55,0-1,0.45-1,1v0.553L9,0.555C8.727,0.297,8.477,0,8,0S7.273,0.297,7,0.555
-    L0.55,7C0.238,7.325,0,7.562,0,8c0,0.563,0.432,1,1,1h1v6c0,0.55,0.45,1,1,1h3v-5c0-0.55,0.45-1,1-1h2c0.55,0,1,0.45,1,1v5h3  c0.55,0,1-0.45,1-1V9h1c0.568,0,1-0.437,1-1C16,7.562,15.762,.325,15.45" 
-  className="fa-secondary"></path></svg>
-
+<div>
+Mayur
 </div>
-
-
-
-
-<div className="footer-content">
-
-  <svg height="30px" id="Layer_1" version="1.1" viewBox="0 0 16 16" width="30px"
-  fill="currentColor" >
-  <path d="M15.45,7L14,5.551V2c0-0.55-0.45-1-1-1h-1c-0.55,0-1,0.45-1,1v0.553L9,0.555C8.727,0.297,8.477,0,8,0S7.273,0.297,7,0.555
-    L0.55,7C0.238,7.325,0,7.562,0,8c0,0.563,0.432,1,1,1h1v6c0,0.55,0.45,1,1,1h3v-5c0-0.55,0.45-1,1-1h2c0.55,0,1,0.45,1,1v5h3  c0.55,0,1-0.45,1-1V9h1c0.568,0,1-0.437,1-1C16,7.562,15.762,.325,15.45" 
-  className="fa-secondary"></path></svg>
-  
 </div>
 </div>
 </main>
--- src/Components/pages/homepage.js
+++ src/Components/pages/homepage.js
@@ -19,9 +19,9 @@ Hi. My name is Mayur. I am a 23 years old Mechancial Design Engineer. Currently 
 I graduated from Panjab University with Bachelors in Mechancial Engineering. In the past I've been a video editor, a graphic
 designer, a theatre actor and an esports journalist. These days I am more interested in everything revolving around tech. I like to
 code, read books and play videogames in my free time. I plan to work in tech one day. This website is the first step towards it. Please feel
-free to hit me up on any of the social validation platforms 
+free to hit me up on any of the social validation platforms
 <span className="home-bg-scrn-footer"> given below.</span>
-<span className="home-small-scrn-header"> given above.</span>
+<span className="home-small-scrn-header"> given above</span>
 </div>
 </div>
 </main>

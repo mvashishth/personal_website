83b036f4e6c34f083dad0b343958bc76edd0c598
mvashishth <vashishthmayur@gmail.com>
2020-09-30 00:38:10 +0530

    Added Dark Mode, Button Pending

--- src/Components/pages/Projects.js
+++ src/Components/pages/Projects.js
@@ -6,11 +6,13 @@ function Projects() {
 
 
 <projects>
-<body>
-  <div className="col-lg-4 col-lg-offset-4 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 align-items-center text-justify" style={{marginTop:"15vh"}}>
+<body> 
+  <div className="projects col-lg-4 col-lg-offset-4 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 align-items-center text-justify"  style={{marginTop:"15vh"}}>
+
   <img src={image} className="center-block" style={{width:"250px", height:"164.47px", marginBottom:"25px"}} />
   If you are seeing this that means I've been procastinating and not actually working on any projects. I don't know why I fall into this habit but I am try to
   get out of it. It is the reason my wife left me. SARAH PLEASE COME BACK!!! Just kidding...I don't have a wife... Or do I? ( ͡° ͜ʖ ͡°)
+
   </div>
 
 </body>

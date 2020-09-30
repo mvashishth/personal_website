553a9b824387175df9913641929160232d16fc50
mvashishth <vashishthmayur@gmail.com>
2020-10-01 00:34:15 +0530

    package.json update

---
+++ build/precache-manifest.dc49fef0780c41958941064fb5f6958a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505c6976c0dacc95fbd25d47ba6550bf",
+    "url": "/personal_website/index.html"
+  },
+  {
+    "revision": "ba64067f6669c7e69bdf",
+    "url": "/personal_website/static/css/main.c8f60846.chunk.css"
+  },
+  {
+    "revision": "31163c580924aac641f9",
+    "url": "/personal_website/static/js/2.42b12857.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal_website/static/js/2.42b12857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba64067f6669c7e69bdf",
+    "url": "/personal_website/static/js/main.e4f4f2f2.chunk.js"
+  },
+  {
+    "revision": "4af32d432e3667d061b3",
+    "url": "/personal_website/static/js/runtime-main.3cc4455b.js"
+  },
+  {
+    "revision": "3c02e75f669a217a3a6b17a218c361bb",
+    "url": "/personal_website/static/media/main.3c02e75f.jpg"
+  },
+  {
+    "revision": "aa1dceef1b62c467f1f4a63da4cc6cbd",
+    "url": "/personal_website/static/media/none.aa1dceef.png"
+  },
+  {
+    "revision": "7ad7009215d1185b6431e000a7df20a9",
+    "url": "/personal_website/static/media/resume.7ad70092.pdf"
+  }
+]);
